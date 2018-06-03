518ea5c8583f2aac1af138daee72fab66d16fac0
davioitu <30713947+davioitu@users.noreply.github.com>
2018-06-02 23:48:26 -0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,21 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('$pintao'):
+        await client.send_file(message.channel, 'C:\\Users\\davi\Desktop\\Discord\\pintao.png')
+    elif message.content.startswith('$felps'):
+        await client.send_file(message.channel, 'C:\\Users\\davi\Desktop\\Discord\\felps.png')
+
+
+client.run('NDUyNTM5MjAyNzY5Mzg3NTQw.DfSrBA.qSY-v5iWRuim-xpv2_23T6Xd79M')
