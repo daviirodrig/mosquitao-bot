710e9c10c22efa9b9e7a7157ea9a15039b2d1411
davioitu <30713947+davioitu@users.noreply.github.com>
2018-06-03 00:24:41 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,6 +16,8 @@ async def on_message(message):
         await client.send_file(message.channel, 'pintao.png')
     elif message.content.startswith('$felps'):
         await client.send_file(message.channel, 'felps.png')
+    elif message.content.startswith('$ping"):
+        await client.send_message(message.channel, 'Pong!')
 
 
 client.run('NDUyNTM5MjAyNzY5Mzg3NTQw.DfSrBA.qSY-v5iWRuim-xpv2_23T6Xd79M')
