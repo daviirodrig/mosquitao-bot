a8b952260c4f500ccdb6574a01f12be0cbe4bb5d
davioitu <30713947+davioitu@users.noreply.github.com>
2018-06-03 01:00:15 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,6 +10,19 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
+@client.event
+async def on_member_join(member):
+    canal = client.get_channel("297130716985032714")
+    msg = "{} Entrou no Clã Do Mosquito, ae caraiou".format(member.mention)
+    await client.send_message(canal, msg)
+
+
+@client.event
+async def on_member_remove(member):
+    canal = client.get_channel("297130716985032714")
+    msg = "{} Saiu do clã, kkk otário".format(member.mention)
+    await client.send_message(canal, msg)
+
 
 @client.event
 async def on_message(message):
