0a3e14be640007e85a32187d341159b0dbf33688
davioitu <30713947+davioitu@users.noreply.github.com>
2018-06-07 02:03:01 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,6 +30,8 @@ async def on_member_remove(member):
 async def on_message(message):
     if message.author == client.user:
         return
+    elif message.content.startswith('$paz')
+        await client.send_file(message.channel, 'paz.jpg')
     elif message.content.startswith('$pintao'):
         await client.send_file(message.channel, 'pintao.png')
     elif message.content.startswith('$felps'):
