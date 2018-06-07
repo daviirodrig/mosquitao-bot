8a6e2038be253b7a3507bff24b8b7fdd80c689c6
davioitu <30713947+davioitu@users.noreply.github.com>
2018-06-07 19:08:34 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,18 +32,22 @@ async def on_message(message):
         return
     elif message.content.startswith('$paz'):
         await client.send_file(message.channel, 'paz.jpg')
+        print('{}: $paz'.format(message.author))
     elif message.content.startswith('$pintao'):
         await client.send_file(message.channel, 'pintao.png')
+        print('{}: $pintao'.format(message.author))
     elif message.content.startswith('$felps'):
         await client.send_file(message.channel, 'felps.png')
+        print('{}: $felps'.format(message.author))
     elif message.content.startswith('$ping'):
         await client.send_message(message.channel, 'Pong!')
-        print('{} pingou!'.format(message.author))
+        print('{}: $ping'.format(message.author))
     elif message.content.startswith('$pergunta'):
         await client.send_message(message.channel, random.choice(["Sim",
                                                                   "Talvez",
                                                                   "Eu sei lá porra",
                                                                   "Não"]))
+        print('{}: $pergunta'.format(message.author))
 
 
 client.run('NDUyNTM5MjAyNzY5Mzg3NTQw.DfSrBA.qSY-v5iWRuim-xpv2_23T6Xd79M')
