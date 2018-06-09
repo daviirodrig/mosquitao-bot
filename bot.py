0c76f57cae3f6016b63e80538d6feed32db1a74f
davioitu <30713947+davioitu@users.noreply.github.com>
2018-06-09 18:36:21 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,29 +28,30 @@ async def on_member_remove(member):
 
 @client.event
 async def on_message(message):
+    time = message.timestamp
     if message.author == client.user:
         return
     elif message.content.startswith('$laranjo'):
         await client.send_message(message.channel, 'Comando não criado.')
-        print('{}: $laranjo'.format(message.author))
+        print('{} {}: $laranjo'.format(time, message.author))
     elif message.content.startswith('$paz'):
         await client.send_file(message.channel, 'paz.jpg')
-        print('{}: $paz'.format(message.author))
+        print('{} {}: $paz'.format(time, message.author))
     elif message.content.startswith('$pintao'):
         await client.send_file(message.channel, 'pintao.png')
-        print('{}: $pintao'.format(message.author))
+        print('{} {}: $pintao'.format(time, message.author))
     elif message.content.startswith('$felps'):
         await client.send_file(message.channel, 'felps.png')
-        print('{}: $felps'.format(message.author))
+        print('{} {}: $felps'.format(time, message.author))
     elif message.content.startswith('$ping'):
         await client.send_message(message.channel, 'Pong!')
-        print('{}: $ping'.format(message.author))
+        print('{} {}: $ping'.format(time, message.author))
     elif message.content.startswith('$pergunta'):
         await client.send_message(message.channel, random.choice(["Sim",
                                                                   "Talvez",
                                                                   "Eu sei lá porra",
                                                                   "Não"]))
-        print('{}: $pergunta'.format(message.author))
+        print('{} {}: $pergunta'.format(time, message.author))
 
 
 client.run('NDUyNTM5MjAyNzY5Mzg3NTQw.DfSrBA.qSY-v5iWRuim-xpv2_23T6Xd79M')
