ced205ac1e69d1f90642d66e7d8775c9378ae32f
davioitu <30713947+davioitu@users.noreply.github.com>
2018-06-20 21:02:22 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -77,7 +77,7 @@ async def on_message(message):
 
         elif message.content.lower().startswith('$'):
             embed = discord.Embed(title="Comando não encontrado", color=0xff0000)
-            embed.set_author(name="Mosquitão:", url="https://www.mosquitao.glitch.me")
+            embed.set_author(name="IH DEU RUIM", url="https://www.mosquitao.glitch.me")
             embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/459137918318149635.png?v=1")
             await client.send_message(message.channel, embed=embed)
             print('{} {}: {}'.format(tempo, message.author, message.content))
