03256f0723881cf31abbffb0fc9b95f8d747f9a4
davioitu <30713947+davioitu@users.noreply.github.com>
2018-06-24 15:04:51 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 import random
 import time
+import embeds
 
 client = discord.Client()
 
@@ -98,21 +99,7 @@ async def on_message(message):
             print(f'{tempo} {message.author}: {message.content}')
 
         elif message.content.lower().startswith('$help'):
-            embed = discord.Embed(title="", url="https://mosquitao.glitch.me", color=0xc0c0c0)
-            embed.set_author(name="Comandos do bot", url="https://mosquitao.glitch.me",
-                             icon_url="https://goo.gl/Viy31D")
-            embed.add_field(name="Diga [coisas]", value="```Faz o bot dizer coisas```", inline=True)
-            embed.add_field(name="Paz", value="```Mostra a foto de um dedo do meio```", inline=True)
-            embed.add_field(name="Entrar", value="```O bot entra em seu canal de voz```", inline=True)
-            embed.add_field(name="Sair", value="```O bot sai do canal de voz```", inline=True)
-            embed.add_field(name="Pintao", value="```Mostra o pintão do alan```", inline=True)
-            embed.add_field(name="Felps", value="```Mostra uma foto do felps```", inline=True)
-            embed.add_field(name="Ping", value="```Pong!```", inline=True)
-            embed.add_field(name="Limpar [Quantidade]", value="```Limpa uma certa quantidade de mensagens.```", inline=True)
-            embed.add_field(name="Pergunta [Sua pergunta aqui] ", value="```O bot responde perguntas objetivas```",
-                            inline=True)
-            embed.set_footer(text="Prefixo: $")
-            await client.send_message(message.channel, embed=embed)
+            await client.send_message(message.channel, embed=embeds.help)
             print(f'{tempo} {message.author}: {message.content}')
 
         elif message.content.lower().startswith('$'):
