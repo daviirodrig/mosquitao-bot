5658efa8e9448f1b395b2775b5348612f917caeb
davioitu <30713947+davioitu@users.noreply.github.com>
2018-06-23 22:25:39 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -81,10 +81,9 @@ async def on_message(message):
             await client.send_message(message.channel, 'Pong!')
             print(f'{tempo} {message.author}: {message.content}')
 
-        elif message.content.lower().startswith('$pergunta qual o sentido da vida '):
-            await client.send_message(message.channel, '**42**')
-            print(f'{tempo} {message.author}: {message.content}')
         elif message.content.lower().startswith('$pergunta'):
+            if 'sentido da vida' in message.content.lower():
+                await client.send_message(message.channel, '**42**'
             await client.send_message(message.channel, random.choice(["Sim",
                                                                       "Com certeza",
                                                                       "Talvez",
