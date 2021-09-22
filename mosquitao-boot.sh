e6337399becad54677f283174a426e2e064493cf
daviirodrig <30713947+daviirodrig@users.noreply.github.com>
2021-09-21 22:18:06 -0300

    specify python3

--- mosquitao-boot.sh
+++ mosquitao-boot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python3 -m pip install --upgrade -r requirements.txt
-pm2 restart mosquitao --update-env || pm2 start main.py --name=mosquitao --no-autorestart
+pm2 restart mosquitao --update-env || pm2 start "python3 main.py" --name=mosquitao --no-autorestart
