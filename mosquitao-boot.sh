7b2019416b5c01283c85c70c35b1f31014b92f96
daviirodrig <30713947+daviirodrig@users.noreply.github.com>
2021-09-26 23:10:47 -0300

    I regret using pm2

--- mosquitao-boot.sh
+++ mosquitao-boot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python3 -m pip install --upgrade -r requirements.txt
-pm2 restart mosquitao --update-env || pm2 start "python3 main.py" --name=mosquitao --no-autorestart
+screen -dm -L -Logfile mosquitao.log -S "mosquitao" python3 main.py
