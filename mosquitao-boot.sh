eadef6c03d99b08cf223acb6af4fc7feeeba911a
davi <git@davi.gq>
2023-09-10 00:24:15 -0300

    change boot command

--- mosquitao-boot.sh
+++ mosquitao-boot.sh
@@ -2,5 +2,6 @@
 #python3 -m pip install --upgrade -r requirements.txt
 #screen -XS mosquitao quit
 #screen -dm -L -Logfile mosquitao.log -S "mosquitao" python3 main.py
-docker compose up lavalink -d --build
-COMMIT_HASH=$(git rev-parse HEAD) docker compose up bot -d --build
+export COMMIT_HASH=$(git rev-parse HEAD)
+sudo -E docker compose up lavalink -d --build
+sudo -E docker compose up bot -d --build
